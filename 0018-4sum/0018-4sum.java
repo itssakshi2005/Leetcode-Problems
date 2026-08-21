006c4d0f142ce5f0f29713c093201f1dde4b7c10
Sakshi Dubey <139060089+itssakshi2005@users.noreply.github.com>
2026-08-21 19:02:11 +0530

    Time: 22 ms (21.58%), Space: 45.5 MB (88.71%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,76 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length < 4) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        // Fix first number
+        for (int i = 0; i < nums.length - 3; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Fix second number
+            for (int j = i + 1; j < nums.length - 2; j++) {
+
+                // Skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i]
+                             + nums[j]
+                             + nums[left]
+                             + nums[right];
+
+                    if (sum == target) {
+
+                        result.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ));
+
+                        left++;
+                        right--;
+
+                        // Skip duplicate left
+                        while (left < right &&
+                               nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicate right
+                        while (left < right &&
+                               nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+
+                    else if (sum < target) {
+                        left++;
+                    }
+
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
