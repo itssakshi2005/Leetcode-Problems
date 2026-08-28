69ad885086f63525d9d074285d1d12e6c3a40a1e
Sakshi Dubey <139060089+itssakshi2005@users.noreply.github.com>
2026-08-28 19:05:34 +0530

    Time: 0 ms (100%), Space: 45 MB (12.18%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,19 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(target==nums[mid]){
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(target<nums[mid]){
-                end=mid-1;
+            else if(nums[mid]>target){
+                high=mid-1;
             }
             else{
-                start=mid+1;
+                low=mid+1;
             }
         }
-        return start;
+        return low;
     }
 }
