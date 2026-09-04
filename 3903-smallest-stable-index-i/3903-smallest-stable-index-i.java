3d851a73cbdf2c4d7888366cee1cca1119406106
Sakshi Dubey <139060089+itssakshi2005@users.noreply.github.com>
2026-09-04 09:24:13 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.85%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,30 +1,23 @@
 class Solution {
-public int firstStableIndex(int[] nums, int k) {
-int n = nums.length;
-    for (int i = 0; i < n; i++) {
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        // max from 0 to i
-        for (int j = 0; j <= i; j++) {
-            max = Math.max(max, nums[j]);
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        int[] prefixMax=new int[n];
+        int[] suffixMin=new int[n];
+        
+        prefixMax[0]=nums[0];
+        for(int i=1;i<n;i++){
+         prefixMax[i]=Math.max(prefixMax[i-1],nums[i]);
         }
-
-        // min from i to n-1
-        for (int j = i; j < n; j++) {
-            min = Math.min(min, nums[j]);
+        suffixMin[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+        suffixMin[i]=Math.min(suffixMin[i+1],nums[i]);
         }
-
-        int score = max - min;
-
-        if (score <= k) {
-            return i;
-        }
+        for (int i = 0; i < n; i++) { 
+            int score = prefixMax[i] - suffixMin[i]; 
+            if (score <= k) { 
+                return i;
+                 }
+         }
+         return -1;
     }
-
-    return -1;
-}
-
-
 }
